efe7de6c16384896c4dedbbbc98ae23a33f44894
KinjiKawaguchi <kawakin0310@icloud.com>
2023-10-27 14:27:32 +0900

    chore: Create Xcode project

---
+++ EyeGazeDisplay/EyeGazeDisplay/EyeGazeDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeGazeDisplayApp.swift
+//  EyeGazeDisplay
+//
+//  Created by 河口欣仁 on 2023/10/27.
+//
+
+import SwiftUI
+
+@main
+struct EyeGazeDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
