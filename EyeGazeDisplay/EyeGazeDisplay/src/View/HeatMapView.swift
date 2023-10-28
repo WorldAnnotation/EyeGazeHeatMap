f34d2535db5f53ffc954a408603b3b0340539b5e
KinjiKawaguchi <kawakin0310@icloud.com>
2023-10-28 10:47:13 +0900

    feat: Add feature to draw pseudo heatmap over textbook

--- EyeGazeDisplay/EyeGazeDisplay/src/View/HeatMapView.swift
+++ EyeGazeDisplay/EyeGazeDisplay/src/View/HeatMapView.swift
@@ -9,18 +9,26 @@ import SwiftUI
 
 struct HeatMapView: View {
     @ObservedObject var viewModel: HeatMapViewModel
-
+    var a4Image: Image
+    
     var body: some View {
-        ZStack{
-            VStack(spacing: 0) {  // VStackのスペースを0に設定
+        ZStack {
+            // 画像を表示
+            a4Image
+                .resizable()
+                .scaledToFit()
+                .frame(width: (297 * 4.3), height: (210 * 4.3))
+            
+            // ヒートマップを表示
+            VStack(spacing: 0) {
                 ForEach(0..<viewModel.heatmap.rows, id: \.self) {
                     row in
-                    HStack(spacing: 0) {  // HStackのスペースを0に設定
+                    HStack(spacing: 0) {
                         ForEach(0..<viewModel.heatmap.columns, id: \.self) {
                             column in
                             Rectangle()
                                 .fill(viewModel.colorForValue(viewModel.heatmap.data[row][column]))
-                                .frame(width: 35, height: 35)
+                                .frame(width: 12, height: 12)
                                 .onTapGesture{
                                     print("Tapped on cell(\(row), \(column)")
                                 }
--- EyeGazeDisplay/EyeGazeDisplay/src/ViewModel/HeatMapViewModel.swift
+++ EyeGazeDisplay/EyeGazeDisplay/src/ViewModel/HeatMapViewModel.swift
@@ -11,12 +11,14 @@ import SwiftUI
 class HeatMapViewModel: ObservableObject {
     @Published var heatmap: HeatmapData
     
-    init(heatmap:HeatmapData) {
+    init(heatmap: HeatmapData) {
         self.heatmap = heatmap
     }
     
     func colorForValue(_ value: Double) -> Color {
         let normalizedValue = value / 100.0 //正規化
-        return Color(red: normalizedValue, green: 0, blue: 1.0 - normalizedValue)
+        let red = 0.25 + (0.75 * normalizedValue) // 0.25から1.0の範囲で赤を増加させる
+        let green = 0.2 * (1.0 - normalizedValue) // 0.2から0.0の範囲で緑を減少させる
+        return Color(red: red, green: green, blue: 0.0, opacity: normalizedValue)
     }
 }
